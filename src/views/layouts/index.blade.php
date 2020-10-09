5937ad6c575f32ecf1090cbecba1d8d4f2d2237f
cjl <chenjinlong@welaw.com>
2020-10-09 14:37:10 +0800

    dist

---
+++ src/views/layouts/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <meta charset=utf-8>
+    <meta content="IE=edge" http-equiv=X-UA-Compatible>
+    <meta content="width=device-width,initial-scale=1" name=viewport>
+    <title></title>
+    <link href={{URL::asset('xnz_assets/dist/css/chunk-vendors.css')}}
+        rel=stylesheet>
+</head>
+<body>
+<noscript><strong>We're sorry but account-book-fe doesn't work properly without JavaScript enabled. Please enable it to
+        continue.</strong></noscript>
+<div id=app></div>
+<script src={{URL::asset('xnz_assets/dist/js/chunk-vendors.js')}}></script>
+<script src={{URL::asset('xnz_assets/dist/js/app.bundle.js')}}></script>
+</body>
+</html>
